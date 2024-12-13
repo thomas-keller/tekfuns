64d516b959906436528719fd22b550402a083220
Thomas Keller <thomas.e.keller@gmail.com>
2024-12-12 21:19:02 -0500

    Update enrich_figs.R

--- R/enrich_figs.R
+++ R/enrich_figs.R
@@ -111,7 +111,7 @@ rnaplots <- function(dds,pcut=0.05,fcut=2,folder=NULL,fprefix=NULL){
   plen<-50
   cols<- grDevices::colorRampPalette(rev(cols))(plen)
   mat=mat[1:30,]
-  row.names(mat)=res05$csymbol
+  row.names(mat)=res05$csymbol[1:30]
   myBreaks <- c(seq(min(mat), 0, length.out=ceiling(plen/2) + 1),
                 seq(max(mat)/plen, max(mat), length.out=floor(plen/2)))
   p=pheatmap::pheatmap(mat,annotation_col=anno,scale='row',show_rownames=FALSE,silent=TRUE,breaks=myBreaks,color=cols)
